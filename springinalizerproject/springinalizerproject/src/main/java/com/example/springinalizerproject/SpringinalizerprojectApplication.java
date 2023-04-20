f9e1cb5914681e8eab9fba403c66c80700c3b1ba
samplergithubo <131270624+samplergithubo@users.noreply.github.com>
2023-04-20 10:14:26 +0530

    Add files via upload

---
+++ springinalizerproject/springinalizerproject/src/main/java/com/example/springinalizerproject/SpringinalizerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinalizerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinalizerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinalizerprojectApplication.class, args);
+	}
+
+}
---
+++ springinalizerproject/springinalizerproject/src/test/java/com/example/springinalizerproject/SpringinalizerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinalizerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinalizerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
